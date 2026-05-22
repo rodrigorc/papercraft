6f5125527e16433a4014059b3a70f20f1464bac0
Rodrigo Rivas Costa <rodrigorivascosta@gmail.com>
2026-05-22 10:40:32 +0200

    Semaphore tests

--- src/semaphore.rs
+++ src/semaphore.rs
@@ -5,8 +5,12 @@
 //!
 //! The initial count of the semaphore will be the number of cpus times a factor,
 //! 1 by default.
+#![allow(dead_code)]
 
-use std::sync::{Condvar, Mutex};
+use std::{
+    sync::{Condvar, Mutex},
+    time::Duration,
+};
 
 pub struct Semaphore<D> {
     inner: Mutex<Data<D>>,
@@ -52,6 +56,29 @@ impl<D> Semaphore<D> {
         f(&mut inner.data)
     }
 
+    pub fn wait_timeout(&self, count: usize, timeout: Duration) -> Option<()> {
+        self.wait_timeout_with(count, timeout, |_| ())
+    }
+
+    pub fn wait_timeout_with<R>(
+        &self,
+        count: usize,
+        timeout: Duration,
+        f: impl FnOnce(&mut D) -> R,
+    ) -> Option<R> {
+        let inner = self.inner.lock().unwrap();
+        let (mut inner, to) = self
+            .cvar
+            .wait_timeout_while(inner, timeout, |d| d.count < count)
+            .unwrap();
+        if to.timed_out() {
+            None
+        } else {
+            inner.count -= count;
+            Some(f(&mut inner.data))
+        }
+    }
+
     pub fn signal(&self, count: usize) {
         self.signal_with(count, |_| ());
     }
@@ -66,5 +93,69 @@ impl<D> Semaphore<D> {
 
     pub fn into_inner(self) -> D {
         self.inner.into_inner().unwrap().data
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::assert_matches;
+
+    #[test]
+    fn test_count() {
+        let sem = Semaphore::new(2, ());
+        assert_matches!(sem.wait_timeout(1, Duration::ZERO), Some(()));
+        assert_matches!(sem.wait_timeout(1, Duration::ZERO), Some(()));
+        assert_matches!(sem.wait_timeout(1, Duration::ZERO), None);
+
+        sem.signal(1);
+        assert_matches!(sem.wait_timeout(1, Duration::ZERO), Some(()));
+        assert_matches!(sem.wait_timeout(1, Duration::ZERO), None);
+
+        sem.signal(2);
+        assert_matches!(sem.wait_timeout(1, Duration::ZERO), Some(()));
+        assert_matches!(sem.wait_timeout(1, Duration::ZERO), Some(()));
+        assert_matches!(sem.wait_timeout(1, Duration::ZERO), None);
+
+        sem.signal(2);
+        assert_matches!(sem.wait_timeout(3, Duration::ZERO), None);
+
+        assert_matches!(sem.wait_timeout(2, Duration::ZERO), Some(()));
+        assert_matches!(sem.wait_timeout(1, Duration::ZERO), None);
+    }
+
+    #[test]
+    fn test_thread() {
+        struct Tok;
+
+        // Keep the sem count and the vec len the same.
+        let sem = Semaphore::new(4, vec![Tok, Tok, Tok, Tok]);
+        let chrono = std::time::Instant::now();
+        std::thread::scope(|s| {
+            for _ in 0..10 {
+                s.spawn({
+                    let sem = &sem;
+                    move || {
+                        for _ in 0..10 {
+                            // If the semaphore fails to arbitrate, panic!
+                            let tok = sem.wait_with(1, |v| v.pop()).expect("should have a Tok");
+                            std::thread::sleep(Duration::from_millis(10));
+                            sem.signal_with(1, |v| v.push(tok));
+
+                            std::thread::sleep(Duration::from_millis(1));
+                        }
+                    }
+                });
+            }
+        });
+        let elapsed = chrono.elapsed().as_millis();
+
+        dbg!(elapsed);
+        // 10 threads touching tokens 10 times each. Those are 100 touches.
+        // Each touch is 10 ms. Total 1000 ms.
+        // With 4 tokens, it should take a bit over 250 ms (3 tokens would be about 333 ms)
+        // Allow for some imprecision and overloads.
+        // I know, tests that measure time are not pretty, but it works for me...
+        assert!(249 < elapsed && elapsed < 300);
     }
 }
